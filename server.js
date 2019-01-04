610d58c25ab4d59a406fec71775e8406c3eabbe4
AkhlakurRahman <akhlakur.rahmann@gmail.com>
2019-01-05 03:12:03 +0600

    Projects page added

--- server.js
+++ server.js
@@ -10,16 +10,22 @@ hbs.registerHelper('getCurrentYear', () => {
 app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'));
 
-app.get('/', (rep, res) => {
+app.get('/', (req, res) => {
     res.render('index.hbs', {
         pageTitle: 'Home Title',
     });
 });
 
-app.get('/about', (rep, res) => {
+app.get('/about', (req, res) => {
     res.render('about.hbs', {
         pageTitle: 'About Title',
     });
+});
+
+app.get('/projects', (req, res) => {
+    res.render('projects.hbs', {
+        pageTitle: 'Projects Title'
+    })
 });
 
 app.listen(port, () => {
